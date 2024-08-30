188ac119804d7f105ba57198cb03812276549d87
AliyanKhan22 <alyanwasim81@gmail.com>
2024-08-30 15:55:05 -0700

    JavaScript Practice Fastest

---
+++ script.js
@@ -0,0 +1,197 @@
+// Prompts
+// var name = prompt("What Is Your Name ?", "Enter Your Name");
+// console.log(name);
+// var age = prompt("What Is Your Age ?", "Enter Your Age");
+// alert(age)
+
+
+
+
+ // if statements
+//  var x = prompt("Checking?");
+//   if (x === "Ali") {
+//      alert("Correct!");
+//   }
+
+
+
+
+// Comparison operators
+// var ticket = prompt ("Enter Your Ticket Number")
+// if (ticket === "101"){
+//     alert("Correct Ticket Number")
+// }
+
+// if (ticket !== "101"){
+//     alert("Wrong Ticket Number")
+// }
+
+// if (1 > 0) {
+//     alert("Right")
+// }
+
+// if (0 < 1) {
+//     alert("Right")
+// }
+
+// if (1 >= 0) {
+//     alert("Right")
+// }
+
+
+
+
+// if...else and else if statements
+// var x = prompt("Conditions")
+// if (x === "Ali") {
+//  alert("Correct!");
+//  }
+//  else {
+//  alert("Wrong answer");
+//  }
+
+// var x = prompt("Conditions")
+// if (x === "Ali") {
+//  alert("Correct!");
+//  }
+//  else if(x === "Aliyan"){
+//     alert("Correct")
+//  }
+//  else {
+//  alert("Incorrect");
+//  }
+
+
+
+
+// Testing sets of conditions
+// var weight = prompt("Enter Weight")
+// var time = prompt("Enter Your Time")
+//  if (weight > 300 && time < 6) {
+//  alert("Correct!");
+//  }
+//  else {
+//  alert("Incorrect!");
+//  }
+
+// var weight2 = prompt("Enter Weight")
+// var time2 = prompt("Enter Your Time")
+//  if (weight2 > 300 || time2 < 6) {
+//  alert("Correct!");
+//  }
+//  else {
+//  alert("Incorrect!");
+//  }
+
+
+
+
+// Arrays
+// var cities = ["Pakistan", "India", "Bangladesh", "Denver", "Los Angeles", "Seattle"];
+// alert("Welcome to "+cities[0])
+
+
+
+
+// Arrays Adding and removing elements
+// var pets = [];
+// pets[0] = "dog";
+// pets.pop();
+// pets.push("Ali");
+// console.log(pets);
+
+
+
+
+// Arrays Removing, inserting,and extracting elements
+// var pets = ["Ali", "Aliyan", "Khan"];
+// pets.shift()
+// pets.unshift("Ali")
+// console.log(pets);
+
+
+
+
+// for loops
+// var Cities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+// for (var i = 0; i <= 4; i++) {
+//  console.log([i]);
+//  } 
+
+
+
+
+// Generating random numbers
+// var randomNumber = Math.random();
+// console.log(randomNumber);
+
+
+
+
+// Converting strings to integers and decimals
+// var currentAge = prompt("Enter your age.");
+//  var qualifying = parseInt(currentAge) +1;
+//  alert(qualifying)
+
+
+
+
+// Getting the current date and time
+// var rightNow = new Date();
+// document.write(rightNow)
+
+
+
+// Event Listner
+//   var inp = document.getElementById("inpb")
+//   function btnclick(){
+//     console.log(inp.value);
+//   }
+  
+
+
+// Functions
+//   function tellTime() {
+//  var now = new Date();
+//  var theHr = now.getHours();
+//  var theMin = now.getMinutes();
+//  console.log("Current time: "+ theHr + ":" + theMin);
+//  }
+//  tellTime()
+
+
+
+// function greetUser(greeting) {
+//  alert(greeting);
+//  }
+//  greetUser()
+
+
+
+//  function addNumbers() {
+//  theSum = 2 + 2;
+//  alert(theSum);
+//  }
+//  addNumbers()
+
+
+// function abc (){
+//     var a = "Ali"
+//     return ab;
+//     console.log(ab);
+// }
+
+
+
+
+// switch Statements
+// var day = "mon"
+// switch(day){
+// case "mon":
+//     alert("Monday")
+//  break;
+//  case "Tue":
+//  break;
+//  default:
+//  alert("Non")
+// }
